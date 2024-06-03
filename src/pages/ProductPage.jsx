bcb387f3b7fd9e8209551bd301d499fc88742089
John Keno Iweh <iwehjohn@gmail.com>
2024-06-03 23:10:34 +0100

    added header to product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,25 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Products from "../components/products/Products";
 import ProductDetails from "../components/product-details/ProductDetails";
+import { ChevronRight } from "../icons/icons";
 
 export default function ProductPage() {
   const { productId } = useParams();
   return (
     <>
-      <header>ProductPage {}</header>
+      <header className='pdheader'>
+        <div className="pdheadercontainer">
+          <nav><ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+              <div style={{color: 'var(--light-gray)'}}><ChevronRight/></div>
+            <li >
+              <Link to='/' style={{color: 'var(--light-gray)'}}>shop</Link>
+            </li>
+            </ul></nav>
+        </div>
+      </header>
       <section className="sectionPdDetails">
         <ProductDetails id={productId} />
       </section>
