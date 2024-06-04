0f1c50662d8c1144058a7158386dddc765125f8c
John Keno Iweh <iwehjohn@gmail.com>
2024-06-04 10:48:06 +0100

    styled products section in product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -7,23 +7,31 @@ export default function ProductPage() {
   const { productId } = useParams();
   return (
     <>
-      <header className='pdheader'>
+      <header className="pdheader">
         <div className="pdheadercontainer">
-          <nav><ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-              <div style={{color: 'var(--light-gray)'}}><ChevronRight/></div>
-            <li >
-              <Link to='/' style={{color: 'var(--light-gray)'}}>shop</Link>
-            </li>
-            </ul></nav>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <div style={{ color: "var(--light-gray)" }}>
+                <ChevronRight />
+              </div>
+              <li>
+                <Link to="/" style={{ color: "var(--light-gray)" }}>
+                  shop
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <section className="sectionPdDetails">
         <ProductDetails id={productId} />
       </section>
-      <Products style={{ textAlign: "left" }} hideDefaults />
+      <section className="sectionProducts" >
+        <Products style={{ textAlign: "left" }} hideDefaults />
+      </section>
     </>
   );
 }
